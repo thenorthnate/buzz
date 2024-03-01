ad753323f709a162c275cf4af8b6e6ed1bb4c201
Nathan North <nathantnorth@yahoo.com>
2024-02-29 20:03:09 -0800

    Improve tests and fix bugs (#2)

--- middleware_test.go
+++ middleware_test.go
@@ -2,24 +2,23 @@ package buzz
 
 import (
 	"context"
-	"errors"
 	"testing"
 )
 
 func TestRecoveryMiddleware(t *testing.T) {
-	bee := NewWorker(&mockTask{
+	worker := NewWorker(&mockTask{
 		dofunc: func(ctx context.Context) error {
-			return errors.New("darn")
+			panic("darn")
 		},
 	}).Use(RecoveryMiddleware)
-	chain := bee.assembleCallChain()
+	chain := worker.assembleCallChain()
 	if chain.exec == nil {
 		t.Fatal("exec was supposed to be defined but was nil instead")
 	}
 	if chain.next == nil {
 		t.Fatal("chain.next was not supposed to be nil")
 	}
-	if err := bee.runChainOnce(context.Background(), chain); err == nil {
+	if err := worker.runChainOnce(context.Background(), chain); err == nil {
 		t.Fatal("runChainOnce was supposed to return an error but did not")
 	}
 }
